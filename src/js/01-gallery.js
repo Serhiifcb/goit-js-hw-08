59849233ca7e25d9d9a077ec8583d2963f443290
Serhii Moroz <sergiy.m.lutsk@gmail.com>
2022-09-22 12:43:24 +0300

    add task 1 from past hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+const refGallery = document.querySelector('.gallery');
+function createGallery() {
+  const markup = galleryItems
+    .map(
+      item => `<a class="gallery__item" href="${item.original}">
+  <img class="gallery__image" src="${item.preview}" alt="${item.description}" />
+</a>`
+    )
+    .join('');
+  refGallery.insertAdjacentHTML('beforeend', markup);
+}
+createGallery();
+refGallery.addEventListener('click', makeLightbox, { once: true });
+const lightbox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
+function makeLightbox(event) {
+  event.preventDefault();
+}
 console.log(galleryItems);
