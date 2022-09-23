1c8d8997d1f8faacc7b6df4b3b6c67c9ec7a82e5
Serhii Moroz <sergiy.m.lutsk@gmail.com>
2022-09-24 00:39:25 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,10 @@
-
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+let currentTime = 'videoplayer-current-time';
+player.setCurrentTime(localStorage.getItem(currentTime));
+player.on(
+  'timeupdate',
+  throttle(data => localStorage.setItem(currentTime, data.seconds), 1000)
+);
