3bbade9cc35f80b81493e074bf2b09ecf87dd33f
Serhii Moroz <sergiy.m.lutsk@gmail.com>
2022-09-26 00:08:32 +0300

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,17 @@
-
+import throttle from 'lodash.throttle';
+const refForm = document.querySelector('.feedback-form');
+// console.log(refForm);
+refForm.addEventListener('submit', handleSubmit);
+const userData = {
+  email: '',
+  message: '',
+};
+function handleSubmit(event) {
+  event.preventDefault();
+  const inputElements = event.currentTarget.elements;
+  userData.email = inputElements.email.value;
+  userData.message = inputElements.message.value;
+  console.log(userData);
+  // event.currentTarget.reset();
+}
+// console.log(userData);
